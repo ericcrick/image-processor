932020bf999d4d6a5ddd32c47e8ac5f053a31f42
ericcrick <nanaoseieric6@gmail.com>
2022-04-03 07:33:53 +0000

    Added Queue management service to DI

--- ImageProsessing.API/Program.cs
+++ ImageProsessing.API/Program.cs
@@ -7,9 +7,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddScoped<IBlobsManagement,BlobsManagement>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IBlobsManagement,BlobsManagement>();
+builder.Services.AddScoped<IQueuesManagement,QueuesManagement>();
 
 var app = builder.Build();
 
